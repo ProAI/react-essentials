8f269e6766f89b0ef396053b3b9e35e89b54a6f9
Markus J. Wetzel <markuswetzel@gmx.net>
2017-06-17 23:26:07 +0200

    Replace gemini-scrollbar with react-custom-scrollbars

--- src/Scrollbars/Scrollbars.js
+++ src/Scrollbars/Scrollbars.js
@@ -1,75 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import GeminiScrollbar from 'gemini-scrollbar';
+import { Scrollbars as ReactCustomScrollbars } from 'react-custom-scrollbars';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-  style: PropTypes.object,
-  autoShow: PropTypes.bool,
-  forceCustom: PropTypes.bool,
 };
 
-const defaultProps = {
-  className: null,
-  style: null,
-  autoShow: false,
-  forceCustom: false,
-};
-
-class Scrollbars extends React.Component {
-  componentDidMount() {
-    this.scrollbar = new GeminiScrollbar({
-      element: this.container,
-      autoshow: this.props.autoShow,
-      forceGemini: this.props.forceCustom,
-      createElements: false,
-    }).create();
-  }
-
-  componentDidUpdate() {
-    this.scrollbar.update();
-  }
-
-  componentWillUnmount() {
-    if (this.scrollbar) {
-      this.scrollbar.destroy();
-    }
-    this.scrollbar = null;
-  }
-
-  /**
-   * Holds the reference to the GeminiScrollbar instance.
-   * @property scrollbar <public> [Object]
-   */
-  scrollbar = null;
-
-  render() {
-    const { className, children, style } = this.props;
-
-    return (
-      <div
-        className={className}
-        style={style}
-        ref={(c) => {
-          this.container = c;
-        }}
-      >
-        <div className="gm-scrollbar -vertical">
-          <div className="thumb" />
-        </div>
-        <div className="gm-scrollbar -horizontal">
-          <div className="thumb" />
-        </div>
-        <div className="gm-scroll-view">
-          {children}
-        </div>
-      </div>
-    );
-  }
+function Scrollbars({ children, ...attributes }) {
+  return (
+    <ReactCustomScrollbars {...attributes}>
+      {children}
+    </ReactCustomScrollbars>
+  );
 }
 
 Scrollbars.propTypes = propTypes;
-Scrollbars.defaultProps = defaultProps;
 
 export default Scrollbars;
