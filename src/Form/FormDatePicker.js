7daa4c79897db83b3780c02fb8a0c3f910252987
Markus J. Wetzel <markuswetzel@gmx.net>
2017-08-10 13:47:03 +0200

    Update date picker component

--- src/Form/FormDatePicker.js
+++ src/Form/FormDatePicker.js
@@ -1,10 +1,10 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions, jsx-a11y/role-has-required-aria-props */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import DayPicker from 'react-day-picker/DayPicker';
 import Field from './Field';
-import Dropdown from '../Dropdown/Dropdown';
-import injectDropdownToggle from '../Dropdown/injectDropdownToggle';
 
 const propTypes = {
   label: PropTypes.string,
@@ -25,51 +25,165 @@ const defaultProps = {
 
 class FormDatePicker extends React.Component {
   state = {
-    extended: false,
+    isOpen: false,
   };
 
-  onInputFocus = () => {
-    this.setState({
-      extended: true,
-    });
+  componentWillReceiveProps(nextProps) {
+    // if datepicker is not focused, close dropdown
+    if (!nextProps.meta.active && this.state.isOpen) {
+      this.toggle();
+    }
+  }
+
+  componentDidUpdate() {
+    // if field is active, set focus on tabIndex element
+    if (this.props.meta.active) {
+      this.input.focus();
+    }
+
+    // scroll to bottom of menu
+    if (this.state.isOpen) {
+      const menuRect = this.menu.getBoundingClientRect();
+      if (window.innerHeight < menuRect.bottom) {
+        window.scrollBy(0, menuRect.bottom - window.innerHeight);
+      }
+    }
+  }
+
+  onToggleMouseDown = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    // if datepicker is not focused, focus datepicker
+    if (!this.props.meta.active) {
+      this.props.input.onFocus();
+    }
+
+    // open/close dropdown
+    this.toggle();
   };
 
-  onInputBlur = () => {
+  onToggleKeyDown = (event) => {
+    if (!this.state.isOpen && event.key === 'ArrowDown') {
+      this.onToggleMouseDown(event);
+    }
+  };
+
+  onMenuMouseDown = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+  };
+
+  onInputKeyDown = (event) => {
+    if (this.state.isOpen && event.key === 'Escape') {
+      this.toggle();
+    }
+  };
+
+  onDayClick = (day) => {
+    // close dropdown
+    this.toggle();
+
+    // set value
+    this.props.input.onChange(day);
+  };
+
+  toggle = () => {
     this.setState({
-      extended: false,
+      isOpen: !this.state.isOpen,
     });
   };
 
   render() {
     const { label, placeholder, info, size, input, meta } = this.props;
-
     const labelClasses = cx('form-control-label', { active: meta.active });
-
-    const classes = cx('form-control custom-datepicker', {
-      'form-control-danger': meta.error,
-      'form-control-sm': size === 'sm',
+    const classes = cx('form-datepicker Select Select--single', {
+      'form-datepicker-danger': meta.error,
+      'form-datepicker-sm': size === 'sm',
+      'has-value': input.value,
+      'is-focused': meta.active,
+      'is-open': this.state.isOpen,
     });
+    const controlClasses = cx('form-datepicker-control Select-control');
+    const menuClasses = cx('form-datepicker-menu');
 
-    const menuClasses = cx('custom-datepicker-menu');
-
-    const InputToggleDropdown = injectDropdownToggle('input');
+    const pickedDate = input.value ? new Date(input.value) : new Date();
+    const initialMonth = new Date(pickedDate.getFullYear(), pickedDate.getMonth());
 
     return (
       <Field meta={meta} info={info}>
         {label &&
-          <label htmlFor={`${meta.form}-${input.name}`} className={labelClasses}>{label}</label>}
-        <Dropdown>
-          <InputToggleDropdown
-            {...input}
-            id={`${meta.form}-${input.name}`}
-            placeholder={placeholder}
-            type="text"
-            className={classes}
-          />
-          <Dropdown.Menu className={menuClasses}>
-            <DayPicker onDayClick={day => console.log(day)} />
-          </Dropdown.Menu>
-        </Dropdown>
+          <label htmlFor={`${meta.form}-${input.name}`} className={labelClasses}>
+            {label}
+          </label>}
+        <div className={classes}>
+          <div
+            className={controlClasses}
+            onMouseDown={this.onToggleMouseDown}
+            onKeyDown={this.onToggleKeyDown}
+          >
+            <span className="Select-multi-value-wrapper">
+              {!input.value &&
+                <div className="Select-placeholder">
+                  {placeholder}
+                </div>}
+              {input.value &&
+                <div className="Select-value">
+                  <span className="Select-value-label" role="option" aria-selected="true">
+                    {input.value ? pickedDate.toLocaleDateString('en') : ''}
+                  </span>
+                </div>}
+              {/* TODO
+                * aria-owns should be the id of the date list element
+                * aria-activedescendant should be the id of the currently selected day element
+                * aria-readonly should be true if datepicker is disabled
+
+                * see react-select for an example
+              */}
+              <div
+                {...input}
+                ref={(element) => {
+                  this.input = element;
+                }}
+                id={`${meta.form}-${input.name}`}
+                role="combobox"
+                tabIndex="0"
+                className="Select-input"
+                aria-expanded={this.state.isOpen}
+                aria-owns=""
+                aria-activedescendant=""
+                aria-readonly="false"
+                onBlur={() => {
+                  input.onBlur(input.value);
+                }}
+                onKeyDown={this.onInputKeyDown}
+                style={{ border: '0px', width: '1px', display: 'inline-block' }}
+              />
+            </span>
+          </div>
+          {this.state.isOpen &&
+            <div
+              ref={(menu) => {
+                this.menu = menu;
+              }}
+              className={menuClasses}
+              onMouseDown={this.onMenuMouseDown}
+            >
+              {/* TODO
+              * renderDay should be used to highlight the currently selected day
+              * onDayKeyDown should be used to navigate with arrow keys
+            */}
+              <DayPicker
+                initialMonth={initialMonth}
+                selectedDays={pickedDate}
+                onDayClick={this.onDayClick}
+                firstDayOfWeek={1}
+                renderDay={day => day.getDate()}
+                onDayKeyDown={() => {}}
+                enableOutsideDays
+              />
+            </div>}
+        </div>
       </Field>
     );
   }
