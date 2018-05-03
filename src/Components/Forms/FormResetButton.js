9152bff40aee979d9ef13f3906a0cfb1d672b7f7
Markus J. Wetzel <markuswetzel@gmx.net>
2018-05-03 17:55:48 +0200

    Update form buttons

--- src/Components/Forms/FormResetButton.js
+++ src/Components/Forms/FormResetButton.js
@@ -2,23 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Buttons/Button';
 
-const propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
 const contextTypes = {
   formik: PropTypes.object.isRequired,
 };
 
-function FormResetButton({ children, ...props }, { formik }) {
+function FormResetButton({ ...otherProps }, { formik }) {
   return (
-    <Button disabled={!formik.dirty || formik.isSubmitting} onClick={formik.handleReset} {...props}>
-      {children}
-    </Button>
+    <Button
+      {...otherProps}
+      disabled={!formik.dirty || formik.isSubmitting}
+      onClick={formik.handleReset}
+    />
   );
 }
 
-FormResetButton.propTypes = propTypes;
 FormResetButton.contextTypes = contextTypes;
 
 export default FormResetButton;
--- src/Components/Forms/FormSubmitButton.js
+++ src/Components/Forms/FormSubmitButton.js
@@ -2,23 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Buttons/Button';
 
-const propTypes = {
-  children: PropTypes.node.isRequired,
-};
-
 const contextTypes = {
   formik: PropTypes.object.isRequired,
 };
 
-function FormSubmitButton({ children, ...props }, { formik }) {
-  return (
-    <Button disabled={formik.isSubmitting} type="submit" {...props}>
-      {children}
-    </Button>
-  );
+function FormSubmitButton({ ...otherProps }, { formik }) {
+  return <Button {...otherProps} disabled={formik.isSubmitting} type="submit" />;
 }
 
-FormSubmitButton.propTypes = propTypes;
 FormSubmitButton.contextTypes = contextTypes;
 
 export default FormSubmitButton;
