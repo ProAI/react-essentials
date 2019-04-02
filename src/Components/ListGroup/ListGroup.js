6257310822f46f8433f2ad91ea86037fac5d2b39
Markus J. Wetzel <markuswetzel@gmx.net>
2019-04-02 12:32:05 +0200

    Add list-group-horizontal and no-gutters classes

--- src/Components/ListGroup/ListGroup.js
+++ src/Components/ListGroup/ListGroup.js
@@ -7,18 +7,42 @@ import ListGroupItem from './ListGroupItem';
 const propTypes = {
   children: PropTypes.node.isRequired,
   flush: PropTypes.bool,
+  horizontal: PropTypes.bool,
+  horizontalSm: PropTypes.bool,
+  horizontalMd: PropTypes.bool,
+  horizontalLg: PropTypes.bool,
+  horizontalXl: PropTypes.bool,
 };
 
 const defaultProps = {
   flush: false,
+  horizontal: false,
+  horizontalSm: false,
+  horizontalMd: false,
+  horizontalLg: false,
+  horizontalXl: false,
 };
 
-function ListGroup({ children, flush, ...elementProps }) {
+function ListGroup({
+  children,
+  flush,
+  horizontal,
+  horizontalSm,
+  horizontalMd,
+  horizontalLg,
+  horizontalXl,
+  ...elementProps
+}) {
   const classes = cx(
     // constant classes
     'list-group',
     // variable classes
     flush && 'list-group-flush',
+    horizontal && 'list-group-horizontal',
+    horizontalSm && 'list-group-horizontal-sm',
+    horizontalMd && 'list-group-horizontal-md',
+    horizontalLg && 'list-group-horizontal-lg',
+    horizontalXl && 'list-group-horizontal-xl',
   );
 
   return (
--- src/Layout/Grid/Grid.js
+++ src/Layout/Grid/Grid.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import { BaseView } from '../../utils/components';
 import GridBox from './GridBox';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
+  noGutters: PropTypes.bool,
 };
 
-function Grid({ children, ...elementProps }) {
+const defaultProps = {
+  noGutters: false,
+};
+
+function Grid({ children, noGutters, ...elementProps }) {
+  const classes = cx(
+    // constant classes
+    'row',
+    'flex-row',
+    // variable classes
+    noGutters && 'no-gutters',
+  );
+
   return (
-    <BaseView props={elementProps} className="row flex-row">
+    <BaseView props={elementProps} className={classes}>
       {children}
     </BaseView>
   );
 }
 
 Grid.propTypes = propTypes;
+Grid.defaultProps = defaultProps;
 
 Grid.Box = GridBox;
 
