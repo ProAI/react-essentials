eda1f496ac62055944f10b830017a39b0e24f8bd
Markus J. Wetzel <markuswetzel@gmx.net>
2019-05-24 18:31:50 +0200

    Update NavLink component

--- src/Components/Navs/NavLink.js
+++ src/Components/Navs/NavLink.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { Route } from 'react-router';
+import { __RouterContext as RouterContext, matchPath } from 'react-router';
 import BaseTouchable from '../../utils/rnw-compat/BaseTouchable';
 import useActionElement from '../../hooks/useActionElement';
 import action from '../../utils/action';
@@ -12,6 +12,9 @@ const propTypes = {
   children: PropTypes.node.isRequired,
   exact: PropTypes.bool,
   strict: PropTypes.bool,
+  isActive: PropTypes.func,
+  // eslint-disable-next-line react/forbid-prop-types
+  location: PropTypes.object,
 };
 const defaultProps = {
   ...action.defaultProps,
@@ -20,28 +23,44 @@ const defaultProps = {
 };
 
 const NavLink = React.forwardRef(function NavLink(props, ref) {
-  const { exact, strict, ...elementProps } = props;
+  const {
+    exact,
+    strict,
+    isActive: isActiveProp,
+    location: locationProp,
+    ...elementProps
+  } = props;
 
+  const context = useContext(RouterContext);
   const createElement = useActionElement(BaseTouchable, elementProps, ref);
 
   const path = typeof props.to === 'object' ? props.to.pathname : props.to;
 
-  return (
-    <Route path={path} exact={exact} strict={strict}>
-      {({ match }) => {
-        const classes = cx(
-          // constant classes
-          'nav-link',
-          // variable classes
-          match && 'active',
-        );
+  // Regex taken from: https://github.com/pillarjs/path-to-regexp/blob/master/index.js#L202
+  const escapedPath = path && path.replace(/([.+*?=^!:${}()[\]|/\\])/g, '\\$1');
 
-        return createElement({
-          className: classes,
-        });
-      }}
-    </Route>
+  const pathToMatch = locationProp
+    ? locationProp.pathname
+    : context.location.pathname;
+
+  const match = escapedPath
+    ? matchPath(pathToMatch, { path: escapedPath, exact, strict })
+    : null;
+
+  const isActive = !!(isActiveProp
+    ? isActiveProp(match, context.location)
+    : match);
+
+  const classes = cx(
+    // constant classes
+    'nav-link',
+    // variable classes
+    isActive && 'active',
   );
+
+  return createElement({
+    className: classes,
+  });
 });
 
 NavLink.displayName = 'NavLink';
