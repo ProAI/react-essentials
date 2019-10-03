091679e04c280807995cee6c50bab5e5b373f14c
Markus J. Wetzel <markuswetzel@gmx.net>
2019-10-03 18:16:01 +0200

    Improve theme integration

--- src/styling/StyleSheet.js
+++ src/styling/StyleSheet.js
@@ -1,16 +1,32 @@
 import BaseStyleSheet from 'react-native-web/dist/cjs/exports/StyleSheet';
-import Theme from './Theme';
 
-const cache = [];
-let initialized = false;
+let activeThemeKey = null;
 
-function makeStyle(source) {
-  const preparedSource = source;
-  const theme = Theme.get();
+const themes = [];
+const sheets = [];
 
-  Object.entries(source).forEach(([key, value]) => {
-    if (typeof value === 'function') {
-      preparedSource[key] = value(theme);
+function updateThemeKey(source) {
+  const hash = JSON.stringify(source);
+
+  const key = themes.findIndex(theme => theme.hash === hash);
+
+  if (key === -1) {
+    const length = themes.push({ hash, source });
+
+    return length - 1;
+  }
+
+  return key;
+}
+
+function createSheet(sheet) {
+  const theme = themes[activeThemeKey].source;
+  const preparedSource = { ...sheet.source };
+
+  // Apply theme to themeable styles.
+  Object.entries(preparedSource).forEach(([key, createStyle]) => {
+    if (typeof createStyle === 'function') {
+      preparedSource[key] = createStyle(theme);
     }
   });
 
@@ -19,32 +35,62 @@ function makeStyle(source) {
 
 const StyleSheet = {
   create(source) {
-    const sheet = {
-      source,
-      result: initialized ? makeStyle(source) : source,
-    };
-
-    cache.push(sheet);
-
-    return sheet.result;
-  },
-  build(initialTheme: Object) {
-    if (!initialized) {
-      Theme.set(initialTheme);
-
-      cache.forEach((sheet, key) => {
-        Object.assign(cache[key].result, makeStyle(sheet.source));
-      });
+    // If the sheet contains of static styles only, theme changes will not
+    // affect the sheet, so we can just create it, no need for caching.
+    if (!Object.values(source).some(style => typeof style === 'function')) {
+      return BaseStyleSheet.create(source);
     }
 
-    initialized = true;
+    // Create sheet object.
+    const sheet = {
+      source,
+      cache: {},
+      active: null,
+    };
+
+    // If initialized, set active sheet based by active theme.
+    if (activeThemeKey !== null) {
+      sheet.cache[activeThemeKey] = createSheet(sheet);
+      sheet.active = sheet.cache[activeThemeKey];
+    } else {
+      sheet.active = { ...source };
+    }
+
+    sheets.push(sheet);
+
+    return sheet.active;
+  },
+  build(theme: Object) {
+    const themeKey = updateThemeKey(theme);
+
+    // If theme is already set, we don't need to do anything.
+    if (themeKey === activeThemeKey) {
+      return;
+    }
+
+    // Set new theme active.
+    activeThemeKey = themeKey;
+
+    // Update style sheets.
+    sheets.forEach((_, key) => {
+      // If there is no result for the active theme, we'll create a themed
+      // style sheet.
+      if (!sheets[key].cache[themeKey]) {
+        sheets[key].cache[themeKey] = createSheet(sheets[key]);
+      }
+
+      // Assign active object, which is also returned from the create
+      // function, so style changes will be applied on next rerender in
+      // the components as well.
+      Object.assign(sheets[key].active, sheets[key].cache[themeKey]);
+    });
   },
   value(key) {
-    if (!initialized) {
+    if (activeThemeKey === null) {
       throw new Error('Use of StyleSheet.value() before initializing.');
     }
 
-    const theme = Theme.get();
+    const theme = themes[activeThemeKey].source;
 
     return theme[key.substr(1)];
   },
