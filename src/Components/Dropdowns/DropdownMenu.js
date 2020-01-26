c01da48dbb3dd68a23ad998b200aea3c9af10adc
Markus J. Wetzel <markuswetzel@gmx.net>
2020-01-26 01:27:30 +0100

    Add right prop for DropdownMenu

--- src/Components/Dropdowns/DropdownMenu.js
+++ src/Components/Dropdowns/DropdownMenu.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import BaseView from '../../utils/rnw-compat/BaseView';
 
 const propTypes = {
   children: PropTypes.node.isRequired,
+  right: PropTypes.bool,
   triggerId: PropTypes.string,
 };
 
 const defaultProps = {
+  right: false,
   triggerId: null,
 };
 
 const DropdownMenu = React.forwardRef(function DropdownMenu(props, ref) {
-  const { triggerId, ...elementProps } = props;
+  const { right, triggerId, ...elementProps } = props;
+
+  const classes = cx(
+    // constant classes
+    'dropdown-menu',
+    // variable classes
+    right && 'dropdown-menu-right',
+  );
 
   return (
     <BaseView
       {...elementProps}
       ref={ref}
       aria-labelledby={triggerId}
-      essentials={{ className: 'dropdown-menu' }}
+      essentials={{ className: classes }}
     />
   );
 });
